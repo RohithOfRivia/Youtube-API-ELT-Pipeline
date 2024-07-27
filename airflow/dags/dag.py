8a86a1777e39599d24b934efa0f6b7f903ceb7c3
Rohith Kumar Anil <rohithkumaranil@gmail.com>
2024-07-27 13:54:28 -0600

    Update dag.py

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -5,6 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
 
+# Default arguments for the dag
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -15,27 +16,32 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
+# Dag to run everything
 elt_script_dag = DAG(
     default_args=default_args,
     dag_id="elt_script_dag",
     schedule_interval="@daily",
 )
 
-def test_airflow_dag():
-    print("Hello world")
+# Test method for the task that is performed first (Optional)
+# def test_airflow_dag():
+#     print("Hello world")
 
-test_script = PythonOperator(
-    task_id='test',
-    python_callable=test_airflow_dag,
-    dag=elt_script_dag,
-)
+# Method to run the test task
+# test_script = PythonOperator(
+#     task_id='test',
+#     python_callable=test_airflow_dag,
+#     dag=elt_script_dag,
+# )
 
+# Run python script
 run_elt_script = BashOperator(
     task_id='run_elt_script',
     bash_command='python /opt/airflow/scripts/elt_script.py',
     dag=elt_script_dag,
 )
 
+# Run DBT
 run_dbt = DockerOperator(
     task_id='run_dbt',
     image='ghcr.io/dbt-labs/dbt-postgres:1.8.2',
@@ -49,14 +55,14 @@ run_dbt = DockerOperator(
     docker_url='unix://var/run/docker.sock',
     network_mode='bridge',
     mounts=[
-        Mount(source='R:/Projects/Youtube DE/youtube_elt_dbt',
+        Mount(source='Path_to_project_directory/youtube_elt_dbt',
               target='/dbt', type='bind'),
         
-        Mount(source='C:/Users/rohit/.dbt',
+        Mount(source='Path_to_DBT_profiles_directory/.dbt',
               target='/root', type='bind')
     ],
     dag=elt_script_dag
     )
 
-
-test_script >> run_elt_script >> run_dbt
+# Specifying order of operations for the DAG
+run_elt_script >> run_dbt
